d617d117b235b169bfebd7189154a3ed4023d6e1
Justin Joyce <justin.joyce@joocer.com>
2021-07-11 21:23:13 +0100

    like test

---
+++ tests/test_expect_column_values_to_match_like.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import data_expectations as de
+from rich import traceback
+
+traceback.install()
+
+
+def test_expect_column_values_to_match_like():
+
+    test_func = de.Expectations([]).expect_column_values_to_match_like
+
+    assert test_func(row={"string": "surname"}, column="string", like="%name")
+    assert not test_func(row={"string": "main"}, column="string", like="%name")
+    assert test_func(row={"string": None}, column="string", like="%name")
+    assert not test_func(row={"string": None}, column="string", like="%name", ignore_nulls=False)
+    
+
+if __name__ == "__main__":
+
+    test_expect_column_values_to_match_like()
+
+    print("test manually run")
